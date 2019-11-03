4508d63530b55d1b39c3879124a52d2bf75c5ef2
takami228 <m.takami228@gmail.com>
2019-11-04 08:52:25 +0900

    3-3-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,180 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(title: 'Navigation', home: MainScreen()));
-
-class MainScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text('Main Screen'),
+void main() => runApp(MaterialApp(
+      title: 'Navigation',
+      home: Scaffold(
+        appBar: AppBar(title: Text('TapBox')),
+        body: Center(
+          //子で管理するパターン
+          //child: TapBoxA(),
+          //親で管理するパターン
+          //child: ParentWidget(),
+          //親で管理するパターン
+          child: ParentChildrenWidget(),
+        ),
       ),
-      body: GestureDetector(
-          onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (_) => DetailScreen()));
-          },
-          child: Center(
-            child: Hero(
-                tag: 'imageHero',
-                child: Image.network('https://picsum.photos/250?image=9')),
-          )));
+    ));
+
+//ウィジェット自身が状態を管理するパターン
+class TapBoxA extends StatefulWidget {
+  @override
+  _TapBoxAState createState() => _TapBoxAState();
 }
 
-class DetailScreen extends StatelessWidget {
+class _TapBoxAState extends State<TapBoxA> {
+  bool _active = false;
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-      body: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Center(
-            child: Hero(
-                tag: 'imageHero',
-                child: Image.network('https://picsum.photos/250?image=9')),
-          )));
+  Widget build(BuildContext context) => GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+            child: Text(
+          _active ? 'Active' : 'Inactive',
+          style: TextStyle(fontSize: 32.0, color: Colors.white),
+        )),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ));
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+}
+
+//親ウィジェットが状態を管理するパターン
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  @override
+  Widget build(BuildContext context) => Container(
+      // 親が管理するパターン
+      child: TapBoxB(active: _active, onChanged: _handleTapBoxChanged));
+
+  //child: TapBoxC(active: _active, onChanged: _handleTapBoxChanged));
+
+  void _handleTapBoxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+}
+
+class TapBoxB extends StatelessWidget {
+  TapBoxB({Key key, this.active: false, @required this.onChanged})
+      : assert(active != false),
+        assert(onChanged != null),
+        super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+            child: Text(
+          active ? 'Active' : 'Inactive',
+          style: TextStyle(fontSize: 32.0, color: Colors.white),
+        )),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ));
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+}
+
+//親と子の両方で管理するケース
+class ParentChildrenWidget extends StatefulWidget {
+  @override
+  _ParentChildrenWidgetState createState() => _ParentChildrenWidgetState();
+}
+
+class _ParentChildrenWidgetState extends State<ParentChildrenWidget> {
+  bool _active = false;
+
+  @override
+  Widget build(BuildContext context) => Container(
+      child: TapBoxC(active: _active, onChanged: _handleTapBoxChanged));
+
+  void _handleTapBoxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+}
+
+class TapBoxC extends StatefulWidget {
+  TapBoxC({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  _TapBoxCState createState() => _TapBoxCState();
+}
+
+class _TapBoxCState extends State<TapBoxC> {
+  bool _highlight = false;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+            child: Text(
+          widget.active ? 'Active' : 'Inactive',
+          style: TextStyle(fontSize: 32.0, color: Colors.white),
+        )),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(color: Colors.teal[700], width: 10.0)
+              : null,
+        ),
+      ));
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
 }
