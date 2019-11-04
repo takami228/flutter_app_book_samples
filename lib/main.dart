b4fe465590fd7842d95a913382d26134c7de42aa
takami228 <m.takami228@gmail.com>
2019-11-04 20:49:27 +0900

    4-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/counter_app.dart';
+import 'package:flutter_app/counter_reducer.dart';
+import 'package:flutter_app/counter_state.dart';
+import 'package:flutter_app/increment_action.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
-void main() => runApp(MyApp());
+enum Actions { Increment }
 
-class MyApp extends StatelessWidget {
+void main() {
+  final store = new Store<CounterState>(reducers,
+      initialState: new CounterState(counter: 0));
+
+  runApp(FlutterReduxApp(
+    title: 'Flutter Redux Demo',
+    store: store,
+  ));
+}
+
+class FlutterReduxApp extends StatelessWidget {
+  final Store<CounterState> store;
+  final String title;
+
+  FlutterReduxApp({Key key, this.store, this.title}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) => MaterialApp(
+  Widget build(BuildContext context) {
+    return StoreProvider<CounterState>(
+      store: store,
+      child: MaterialApp(
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
+        title: title,
         home: Scaffold(
-            appBar: AppBar(title: Text('Scoped Model')),
-            body: Center(child: CounterApp())),
-      );
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                StoreConnector<CounterState, String>(
+                  converter: (store) => store.state.counter.toString(),
+                  builder: (context, count) {
+                    return Text(
+                      count,
+                      style: Theme.of(context).textTheme.display1,
+                    );
+                  },
+                )
+              ],
+            ),
+          ),
+          floatingActionButton: StoreConnector<CounterState, VoidCallback>(
+            converter: (store) {
+              return () => store.dispatch(IncrementAction());
+            },
+            builder: (context, callback) {
+              return FloatingActionButton(
+                onPressed: callback,
+                tooltip: 'Increment',
+                child: Icon(Icons.add),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
